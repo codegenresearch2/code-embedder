dae233b4dfd37b47901a89335f383f2f44b373ff
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-eu-1>
2025-02-10 13:09:00 +0000

    Reset to 8f449d69ca00b0e785e6b3de2733e4d6065ad572 and made changes for LLM Round 1

--- src/main.py
+++ src/main.py
@@ -1,24 +1,18 @@
 import argparse
-
 from loguru import logger
+from src.code_embedding import CodeEmbedder, ScriptPathExtractor
 
-from src.code_embedding import CodeEmbedder
-from src.script_content_reader import ScriptContentReader
-from src.script_metadata_extractor import ScriptMetadataExtractor
-
-parser = argparse.ArgumentParser()
+parser = argparse.ArgumentParser(description="Process Readme files to embed code.")
 parser.add_argument(
-    "--readme-paths", nargs="+", type=str, help="Paths to Readme files", default="README.md"
+    "--readme-paths", nargs="+", type=str, help="Paths to Readme files", default=["README.md"]
 )
 args = parser.parse_args()
 
 if __name__ == "__main__":
-    script_metadata_extractor = ScriptMetadataExtractor()
-    script_content_reader = ScriptContentReader()
+    script_path_extractor = ScriptPathExtractor()
     code_embedder = CodeEmbedder(
         readme_paths=args.readme_paths,
-        script_metadata_extractor=script_metadata_extractor,
-        script_content_reader=script_content_reader,
+        script_path_extractor=script_path_extractor,
     )
     code_embedder()
     logger.info("Code Embedder finished successfully.")
